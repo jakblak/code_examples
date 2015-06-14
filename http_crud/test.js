b3a99778f90965cb9885e550dd040769d1ba1170
Patrick Schroeder <schroe9@gmail.com>
2015-06-13 23:46:28 -0700

    initial commit

---
+++ http_crud/test.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope', '$http', '$location', '$alert'];
+
+  function MainCtrl($scope, $http, $location, $alert) {
+
+    $scope.alert = {
+      "placement": "top",
+      "title": "Success!",
+      "content": "New thing has been added",
+      "type": "success",
+      "animation": "am-fade-and-slide-top"
+    };
+
+    // GET all the things and show them
+    $http.get('/api/things', {
+      cache: true
+    })
+      .success(function(data) {
+        $scope.things = data;
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+
+    $scope.create = function() {
+      $http.post('/api/things', {
+        name: $scope.name,
+        info: $scope.info
+      })
+        .success(function(data) {
+            $scope.things.push(data);
+            $scope.name = '';
+            $scope.info = '';
+            console.log(data);
+            //$location.url('/#things');
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id)
+        .success(function(data) {
+          $scope.things.splice(data, 1);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+})();
